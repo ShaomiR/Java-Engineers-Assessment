3de8053f0339175a30d9b757bea6acb53eb72a7d
Shaomi Rahman <35505298+ShaomiR@users.noreply.github.com>
2019-08-19 00:43:14 +0600

    Updated Solution.Java

--- src/main/java/com/datasoft/javaengineersassessment/solution/Solution.java
+++ src/main/java/com/datasoft/javaengineersassessment/solution/Solution.java
@@ -1,17 +1,33 @@
 package com.datasoft.javaengineersassessment.solution;
 
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+
 public class Solution implements Runnable{
-	
-	
-	/**
-	 * Application entry to your solution
-	 *
-	 * @see Thread#run()
-	 */
-	@Override
-	public void run() {
-		System.out.println("All set ...");
-		System.out.println("Goodbye :)");
-	
+    @Override
+    public void run() {
+        System.out.println("All set ...");
+        //take the number of test cases in inputNo
+        int testNo = Integer.parseInt(IO.readLine());
+        for (int i = 0;i<testNo;i++)
+        {
+            //take the input of nT and nD in n array
+            String [] stringN = IO.readLine().split(" ");
+            int [] n = new int[stringN.length];
+            for(int j = 0; j<n.length; j++){
+                n[j] = Integer.parseInt(stringN[j]);
+            }
+            //take input of data objects of nT numbers
+            String [] dataObjects = IO.readLine().split(" ");
+            //create a JSON Array which will store the JSON Objects
+            JSONArray ja = new JSONArray();
+            //considering the inputs will be in this format 'name:L. Messi' 
+            //and the input stream will be ended by a new line for each JSON object
+            for (int j =0; j< n[1];i++){
+                //each data in JSONArray will be a JSON Object
+                JSONObject jo = new JSONObject()
+            }
+            System.out.println("Goodbye :)");
 	}
+    }
 }
